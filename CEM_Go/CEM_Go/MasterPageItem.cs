22f268be7db6e0ab62740a51e7268eb4e8289626
BILLAUD <loic.billaud@cgi.com>
2016-08-10 10:44:15 +0200

    Commit project

---
+++ CEM_Go/CEM_Go/MasterPageItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Colimo.Model
+{
+    internal class MasterPageItem
+    {
+        public Type TargetType { get; internal set; }
+
+        public string IconSource { get; set; }
+
+        public string Title { get; internal set; }
+    }
+}
